af7b8f04e52568b7df28461d8a91b9792ff038ed
Trinhnp <trinhnp3@gmail.com>
2019-11-12 18:57:23 -0700

    Updates

---
+++ precache-manifest.dd697623b5e45f016bdd07832f788f29.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d5fe12b88746fdcb361aa9abbafefa2f",
+    "url": "/weather_forecast/index.html"
+  },
+  {
+    "revision": "d788a6227b0a80c276ee",
+    "url": "/weather_forecast/static/css/main.52fc0e45.chunk.css"
+  },
+  {
+    "revision": "015761280cdc09bd186b",
+    "url": "/weather_forecast/static/js/2.8b4dbd39.chunk.js"
+  },
+  {
+    "revision": "d788a6227b0a80c276ee",
+    "url": "/weather_forecast/static/js/main.d4b8012b.chunk.js"
+  },
+  {
+    "revision": "c5c4d3e61028a0356d35",
+    "url": "/weather_forecast/static/js/runtime-main.5bcf50f6.js"
+  }
+]);
